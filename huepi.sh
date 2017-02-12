af84ac604838e2c1d409a4cdc7bb9913c25de6a3
André Schneider <snyd0r@gmail.com>
2017-02-12 18:33:17 +0100

    Prealpha Nothing really works. POC was ok - so the work can go on. There is Problem on Line 54 - but why?

--- huepi.sh
+++ huepi.sh
@@ -2,40 +2,68 @@
 set -x
 
 # Variables
+#Hue Executable
+HUEBIN=/usr/bin/hue
 # Hue Settings
-HUE=""
-BRI=""
-SAT=""
+HUE="7676"
+BRI="144"
+SAT="199"
 #Friends of Hue Settings (Iris, Bloom)
-FOHUE=""
-FOBRI=""
-FOSAT=""
+FOHUE="12057"
+FOBRI="144"
+FOSAT="143"
 
 # Section to define how to check presence of the phones
 # iPhonemarker => BluetoothMac
 IPHONE7BTMAC='70:70:0D:98:A6:9C'
+PRESENCEFILEIPHONE7="/tmp/presenceiphone7"
 # S7-Marker => IP Address (Lease is two Weeks)
 S7IP='192.168.77.49'
 
+function main {
 while :
-    do
-        IsMyiPhoneAtHome=`sudo l2ping -s 1 -c 1 $IPHONE7BTMAC >/dev/null 2>&1; echo $?` #Pruefung, ob das Phone erreichbar ist
-        if [ "$IsMyiPhoneAtHome" = 0 ];
-            then #Phone visible via BT
-               	echo "iPhone is visible"
-                hue set 10 --on
-            else #Phone not visible via BT
-                echo "iPhone is not at home"
-                hue set 10 --off
-        fi
-        sleep 5s
-   done
+do
+  IsMyiPhoneAtHome=`sudo l2ping -s 1 -c 1 $IPHONE7BTMAC >/dev/null 2>&1; echo $?` #Check if iPhone is reachable
+  if [ "$IsMyiPhoneAtHome" = 0 ];
+  then #Phone visible via BT
+  echo "iPhone is visible"
+  turnOnLightWhenArriving
+else #Phone not visible via BT
+  echo "iPhone is not at home"
+  turnOffLightsWhenLeaving
+fi
+sleep 5s
+done
+}
 
-# function turnOnLightWhenArriving
-# hue transit 2,5,9,10 5 --hue 7688 --bri 144 --sat 199 --on
+# Declare used functions
+function turnOnLightWhenArriving {
+  $HUEBIN transit 2,10 5 --hue $HUE --bri $BRI --sat $SAT --on # Turn on original Hue Lights (Bulbs, Strips)
+  $HUEBIN transit 5,9 5 --hue $FOHUE --bri $FOBRI --sat $FOSAT --on # Turn on Friends of Hue lights (Iris, Bloom)
+}
 
-# function isLightAllreadyOn
+function turnOffLightsWhenLeaving {
+  # If Phones are leaving, turn off the lights
+  sleep 60 # Maybe the phones are not away? Maybe they reconnect?
+  # hue transit all 5 --off
+  $HUEBIN transit 10 5 --off
+}
+
+function checkIfLightsAreOn{
+  # Check if any of the lights in "Wohnzimmer" are activated.
+  IsThereLight=`hue get 2,3,5,9,10 | grep -i '"on":true' >/dev/null 2>&1; echo $?`
+  if [[ "$IsThereLight" = 0 ]]; then # 0 = There is light, 1 = there is no light
+  # Lights are allready on
+  echo "Lights are on"
+else
+  # Turn on lights
+  turnOnLightWhenArriving
+fi
+}
 
 # function writePresenceFile
 
 # function isSomebodyhere
+
+# Run this script
+main
