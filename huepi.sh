270002f8c7d41e3c864d7eb19b929815e12ba06a
André Schneider <snyd0r@gmail.com>
2017-02-20 19:41:00 +0100

    First working version! Everything works fine during tests.

--- huepi.sh
+++ huepi.sh
@@ -6,15 +6,15 @@ PHONES=0
 # Hue Executable
 HUEBIN=/usr/bin/hue
 # Hue Settings
-DEVICES="10"
-# DEVICES="2,10"
+# DEVICES="10"
+DEVICES="2,10"
 HUE="7676"
 BRI="144"
 SAT="199"
 # Friends of Hue Settings (Iris, Bloom)
 # They differ in ColorSettings
-FOH_DEVICES="9"
-# FOH_DEVICES="5,9"
+# FOH_DEVICES="9"
+FOH_DEVICES="5,9"
 FOH_HUE="12057"
 FOH_BRI="144"
 FOH_SAT="143"
@@ -30,8 +30,8 @@ IDENTIFIER_S7EDGE='192.168.77.49'
 PRESENCEFILE_S7EDGE="/tmp/presence_s7edge"
 
 # Counters
-# Enable or disable automatic mode
-AUTO_MODE=1 # 1 = on, 0 = off
+TURN_OFF_COUNT=0 # After sending the OFF Signal for 3 times it's enough.
+
 
 function main {
   while true
@@ -39,17 +39,16 @@ function main {
     # Check if any of the lights in "Wohnzimmer" are activated.
     IsThereLight=`hue get 9,10 | grep -i '"on":true' >/dev/null 2>&1; echo $?`
     # IsThereLight=`hue get 2,3,5,9,10 | grep -i '"on":true' >/dev/null 2>&1; echo $?`
-    if [[ "$IsThereLight" = 0 ]]; then # 0 = There is light, 1 = there is no light
+    if [[ "$IsThereLight" = 0 ]]; then # 0 = There is light, 1 = there is no light <- && [[ "$AUTO_MODE" = 1 ]]
     # Lights are allready on
     echo "-!- huepi: Lights are on - waiting for phones to leave." | logger
     while true ; do
       checkForPhones
-      [ "$?" -eq 0 ] && AUTO_MODE=1 && break # If return Value = 0 set to Automatic Mode and break the while.
-      sleep 5s # turn this up in production
+      [ "$?" -eq 0 ] && break # If return Value = 0 break out of the while loop.
+      sleep 15s # turn this up in production
     done
   else
-    echo "-!- huepi: No lights on. Check what to do."
-    # Something has to happen here - but i can't concentrate O_O
+    echo "-!- huepi: No lights on. Lets see what to do." | logger
   fi
   # Check if phones are connected
   checkForPhones
@@ -88,23 +87,27 @@ function showtime() {
   # INDEX = PHONECOUNT
   INDEX=$1
   case "$INDEX" in
-    1)  echo "-!- huepi: iPhone is here." | logger
+    1)  echo "-!- huepi: iPhone is here. Turn the lights on." | logger
     turnOnLight "arriving"
-    AUTO_MODE = 0
+    AUTO_MODE=0
+    TURN_OFF_COUNT=0
     ;;
-    2)  echo "-!- huepi: s7edge is here" | logger
+    2)  echo "-!- huepi: s7edge is here. Turn the lights on." | logger
     turnOnLight "arriving"
-    AUTO_MODE = 0
+    AUTO_MODE=0
+    TURN_OFF_COUNT=0
     ;;
     3)  echo "-!- huepi: iPhone and s7edge are here" | logger
     echo "-!- huepi: We don't want to change activated light settings. Otherwise somebody may get mad ;)" | logger
-    AUTO_MODE = 0
+    AUTO_MODE=0
     ;;
     0) echo "-!- huepi: No phones around" | logger
-    echo "-!- huepi: Grace Time before turn off." | logger
-    echo "-!- huepi: Turn off lights 'cause nobody is home.'" | logger
-    turnOffLightsWhenLeaving
-    AUTO_MODE = 1
+    echo "-!- huepi: Turn off lights because nobody is home." | logger
+    if [[ "$TURN_OFF_COUNT" -le 2 ]]; then
+      turnOffLightsWhenLeaving
+      ((TURN_OFF_COUNT+=1))
+      AUTO_MODE=1
+    fi
     ;;
   esac
 
@@ -120,7 +123,8 @@ function turnOnLight() {
 
 function turnOffLightsWhenLeaving {
   # If Phones are leaving, turn off the lights
-  $HUEBIN transit $DEVICES,$FOH_DEVICES $TRANSITTIME --off
+  $HUEBIN transit all $TRANSITTIME --off
+  # $HUEBIN transit $DEVICES,$FOH_DEVICES $TRANSITTIME --off
 }
 
 function writePresenceFile() {
